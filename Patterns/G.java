f28756d052bf054692500066e40a47407bbea5b1
Dhinesh-Ramachandran <dhineshram73@gmail.com>
2025-06-16 19:43:12 +0530

    Create G.java

---
+++ Patterns/G.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=n-(row-1);col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
